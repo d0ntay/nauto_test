e42bbb4a369eeaf2f6b4b6f789b8c561339c0d1e
d0ntay <d0ntay@d0ntay.com>
2026-05-28 14:46:05 -0400

    new job

--- nautobot-docker-compose/jobs/ping_job.py
+++ nautobot-docker-compose/jobs/ping_job.py
@@ -1,5 +1,6 @@
 from nautobot.apps.jobs import Job, ObjectVar, register_jobs
-from nautobot.dcim.models import Device
+from nautobot.dcim.models import Location, Device
+import socket
 
 name = "test"
 
@@ -8,12 +9,22 @@ class PingJob(Job):
         name = "Device Reachability Test"
         description = "This job pings all the devices in the database."
 
-    device = ObjectVar(model=Device, required=True)
+    location = ObjectVar(model=Location, required=True)
 
-    def run(self, device):
-        self.logger.info(f"Device {device.name}")
+    def ping(self, ip):
+        try:
+            socket.create_connection((ip, 22), timeout=2)
+            return True
+        except OSError:
+            return False
 
-
-
+    def run(self, location):
+        devices = Device.objects.filter(location=location, primaryipv4__isnull = False)
+        for device in devices:
+            ip = str(device.primaryipv4.address.ip)
+            if self.ping(ip):
+                self.logger.info(f"{device.name} ({ip}) is reachable")
+            else:
+                self.logger.warning(f"device.name ({ip}) is unreachable")
 
 register_jobs(PingJob)
